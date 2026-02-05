d1fd1cc07eefb709f119adfd2b62e71fa34817e9
Karoline <karolinemunhozcontato@gmail.com>
2026-02-05 00:33:57 -0300

    construindo os estilos dos temas

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -8,14 +8,26 @@ export const Descricao = styled(P)`
 export const BotaoTema = styled.button`
   border-radius: 12px;
   padding: 8px;
-  color: #eee;
+  color: ${(props) => props.theme.corDeFundo};
   font-size: 10px;
   font-weight: bold;
-  background-color: #282a35;
+  background-color: ${(props) => props.theme.corPrincipal};
   cursor: pointer;
+  border: none;
+  transition: background-color 0.3s;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    top: 16px;
+    right: 16px;
+    z-index: 1000;
+  }
 `
 export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+  @media (max-width: 768px) {
+    text-align: center;
+  }
 `
---
+++ src/themes/dark.ts
@@ -0,0 +1,9 @@
+const temaDark = {
+  corPrincipal: '#fff',
+  corSecundaria: '#eee',
+  corDeFundo: '#282A35',
+  corDeFundoBotao: '#fffF',
+  corDaBorda: '#C1C1C1'
+}
+
+export default temaDark
