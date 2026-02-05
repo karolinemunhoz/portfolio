6e0ab3bccf17064624b342a76206d7b380ddfe21
Karoline <karolinemunhozcontato@gmail.com>
2026-02-04 21:49:26 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/karolinemunhoz.png" alt="Imagem do perfil" />
-    <Titulo fontSize={20}>Karoline Munhóz</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Karoline Munhóz</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        karolinemunhoz
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheira front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
